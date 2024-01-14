f99b5056ec695984b6afcfb6cf9b87732e6223f7
jeremy-po <jeremypoliver@hotmail.com>
2024-01-13 20:55:56 -0400

    prep2 complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,6 +6,7 @@ class Program
     {
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
+        
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        char letter = ' ';
+
+        if (gradePercentage >= 90)
+        {
+            letter = 'A';
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = 'B';
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = 'C';
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine($"You passed with a grade of {letter}.");
+        }
+        else
+        {
+            Console.WriteLine($"You didn't pass this time, but you can do better next time.");
+        }
+
+
+        Console.WriteLine($"Your final grade is: {letter}");
     }
 }
