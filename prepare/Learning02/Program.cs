c0164e53b04f810c353e98cae43bf489ff538820
jeremy-po <jeremypoliver@hotmail.com>
2024-01-17 21:43:14 -0400

    prepare w2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Parts Advisor";
+        job1._company = "JPOAuto";
+        job1._startYear = 2017;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Sales";
+        job2._company = "Motorist";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jeremy Oliver";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
