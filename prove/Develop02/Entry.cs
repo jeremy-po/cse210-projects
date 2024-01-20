ac25ce22f33f88a701c13a9b695e4477c1bbd826
jeremy-po <jeremypoliver@hotmail.com>
2024-01-20 18:27:03 -0400

    exceed requirements by allowing more info to input

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -5,23 +5,26 @@ class Entry
     private string _date;
     private string _prompt;
     private string _entryText;
+    private string _location;
 
-    public Entry(string date, string prompt, string entryText)
+    public Entry(string date, string prompt, string entryText,string location)
     {
         _date = date;
         _prompt = prompt;
         _entryText = entryText;
+        _location = location;
     }
 
     public void Display()
     {
         Console.WriteLine($"Date: {_date}");
         Console.WriteLine($"Prompt: {_prompt}");
+        Console.WriteLine($"Location: {_location}");
         Console.WriteLine($"Entry: {_entryText}\n");
     }
 
     public string GetSaveString()
     {
-        return $"{_date}|{_prompt}|{_entryText}";
+        return $"{_date}|{_prompt}|{_entryText}{_location}";
     }
 }
