45333ad86e089ac1f455d17b4bf9ec83cdc12d17
jeremy-po <jeremypoliver@hotmail.com>
2024-02-08 19:19:00 -0400

    prep05

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string colour, double radius) : base (colour)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 6);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Blue", 8, 10);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green", 12);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string colour = s.GetColour();
+            double area = s.GetArea();
+            Console.WriteLine($"The {colour} shape has an area of {area}.");
+        }
     }
 }
